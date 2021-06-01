0f9631c1a4ddcc9a4dd5eaadc17d9be26aba11a2
pguyal <pguyal@hotmail.com>
2021-06-01 15:14:31 -0400

    Create bird model
    Created bird model with name, species, location, and owner properties.

---
+++ app/models/bird.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const birdSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  species: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Bird', birdSchema)
